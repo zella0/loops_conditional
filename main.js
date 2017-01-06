8054a6e170e45ca6cfd1adcb3c0ace5b22fcc293
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-01-06 12:04:19 -0700

    added main.js file with questions

---
+++ main.js
@@ -0,0 +1,25 @@
+// 1. Write a for loop that will print only the even numbers of this array: [5, 20, 11, 42, 2, 19]
+
+// Your code here...
+
+// 2. Write a for loop that doubles each number in the following array. If the resulting number is greater than 12, print it to the console: [9, 2, 10, 4, 7, 6]
+
+// Your code here...
+
+// 3. Write a while loop that prints every number from 1 - 10
+
+// Your code here...
+
+// 4. Write a for loop to iterate over every element in the following array: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+  // Using a switch statement, do the following:
+
+  // If the day is on a weekend, print "Yay, it's the weekend!"
+
+  // If the day is Friday, print "Almost there..."
+
+  // If the day is Monday, print "I hate Mondays more than Garfield."
+
+  // Otherwise, print "Gotta keep grinding."
+
+// Your code here...
