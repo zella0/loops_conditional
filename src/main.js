ccdd523f230e6b8a3432036a6c803889807fb4d9
Nhan Phan <xeith1712@hotmail.com>
2018-07-16 18:16:33 -0700

    clearing chckpts

--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
 // Write a for loop that will print only even numbers from an array:
 // For example: question0([ 5, 20, 11, 42, 2, 19 ]) >> [ 20, 42, 2 ]
 
-function question0 (array) {
-  // Write your code here! Make sure not to modify the function
-  // declaration above or the `module.exports` code below
+function question0(array) {
+  return array.filter((item) => {
+    return item % 2 === 0;
+  })
 }
 
 // Write a for loop that doubles each number in the following array. If the
 // resulting number is greater than 12, print it to the console:
 // For example: [ 2, 4, 8, 16 ] would print the numbers 16 and 32
 
-function question1 (array) {
-  // Write your code here! Make sure not to modify the function
-  // declaration above or the `module.exports` code below
+function question1(array) {
+  let result = [];
+  array.map((item) => {
+    if (item * 2 > 12) {
+      result.push(item *= 2);
+    }
+  })
+  return result;
 }
 
 // Write a for loop to iterate over every element in the following array:
@@ -24,9 +30,28 @@ function question1 (array) {
 // - If the day is Monday, print "I hate Mondays more than Garfield."
 // - Otherwise, print "Everybody's working for the weekend."
 
-function question2 (array) {
-  // Write your code here! Make sure not to modify the function
-  // declaration above or the `module.exports` code below
+function question2(array) {
+  return array.map((item) => {
+    switch (item) {
+      case 'Mon':
+        return "I hate Mondays more than Garfield.";
+        break;
+      case 'Fri':
+        return "Almost there...";
+        break;
+      case 'Sat':
+        return "Yay, it's the weekend!"
+        break;
+      case 'Sun':
+        return "Yay, it's the weekend!"
+        break;
+      default:
+        return "Everybody's working for the weekend.";
+    }
+  })
 }
-
-module.exports = { question0, question1, question2 }
+module.exports = {
+  question0,
+  question1,
+  question2
+}
